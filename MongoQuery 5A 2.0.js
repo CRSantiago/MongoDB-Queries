d59efb19411c2ff076c3153db9449f8b952f13c9
Christopher Santiago <42612374+CRSantiago@users.noreply.github.com>
2019-04-03 10:37:52 -0400

    Add files via upload

---
+++ MongoQuery 5A 2.0.js
@@ -0,0 +1,6 @@
+db.getCollection('planes').aggregate([
+    {$match: {ground_speed: {$gte: 1}}},
+    {$group: {_id:{aicraft: "$aircraft_id", speed: "$ground_speed"}}},
+    {$sort:{"_id.speed":-1}}, 
+    {$limit:5}
+])
